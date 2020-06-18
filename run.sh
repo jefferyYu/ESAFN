1046e7dcd3d9fe27b16ce32d46585b6146acf3bb
jefferyYu <jefferyYu@users.noreply.github.com>
2020-06-18 18:48:27 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ do
     for k in 'mmfusion' # 'mmram' 'mmmgan' 'mmfusion'
     do
         CUDA_VISIBLE_DEVICES=0 python train.py --dataset ${d} --embed_dim 100 --hidden_dim 100 --model_name ${k} \
-        --att_mode vis_concat_attimg_gate >> ${d}_log.txt
+        --att_mode vis_concat_attimg_gate
         # test --load_check_point >> ${d}_log.txt
     done
 done
